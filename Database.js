fc2cb227e7731eebbcb41d78e5b7659fac8f4288
luiggigarcia <luiggipg2908@gmail.com>
2022-03-18 14:19:51 -0300

    feat: finally tests

--- Database.js
+++ Database.js
@@ -1,22 +1,58 @@
-const { readFile } = require('fs');
-const { promisify } = require('util');
-const readFileSync = promisify(readFile);
-
+const { readFileSync, writeFileSync } = require('fs');
 
 class Database {
     constructor() {
-        this.fileName = './persons.json';
+        this.fileName = 'persons.json';
     }
 
-    async getFileData() {
-        const data = await readFileSync(this.fileName, { encoding: "utf8" });
-        return JSON.parse(data.toString());
+    readFile() {
+        const data = readFileSync(this.fileName, { encoding: 'utf8' });
+        return JSON.parse(data);
     }
 
-    async showPerson(id) {
-        const data = await this.getFileData();
-        return data.filter(person => id ? person.id === id : true);
+    writeFile(data) {
+        writeFileSync(this.fileName, JSON.stringify(data, undefined, "  "));
     }
+
+    showPerson(id) {
+        const data = this.readFile();
+        const person = data.find(person => person.id === id);
+        return person;
+    }
+
+    createPerson(person) {
+        const data = this.readFile();
+        data.push(person);
+        this.writeFile(data);
+        return true;
+    }
+
+    updatePerson(id, dataObject) {
+        const data = this.readFile();
+        const index = data.findIndex(person => person.id === id);
+        if (index === -1) throw Error("Person not found");
+        data[index] = {
+            ...data[index],
+            ...dataObject
+        }
+        this.writeFile(data);
+        return true;
+    }
+
+    deletePerson(id) {
+        const data = this.readFile();
+        if (!id) {
+            data.splice(0);
+            this.writeFile(data);
+        }
+        const index = data.findIndex(person => person.id === id);
+        if (index === -1) throw Error("Person not found");
+        data.splice(index, 1);
+        this.writeFile(data);
+        return true;
+    }
+
 }
 
 module.exports = new Database();
+
--- test.js
+++ test.js
@@ -1,17 +1,37 @@
-const { deepEqual, ok} = require('assert');
+const { deepEqual } = require('assert');
 const Database = require('./Database');
 
-const example = {
-    id: 1,
-    name: "Beatriz Merlin",
-    age: 22
+const expected = {
+    id: 6504,
+    name: "Rafaela"
 };
 
-describe("CRUD less Database", () => {
-    it("Should show the person information by id", async () => {
-        const expected = example;
-        const [result] = await Database.showPerson(expected.id);
+describe("No Database Crud in Node.js", () => {
+    before(() => {
+        Database.createPerson(expected);
+        Database.createPerson({
+            id: 5,
+            name: "Baby"
+        })
+    });
+
+    it("Should show the person by id", () => {
+        const result = Database.showPerson(6504);
         deepEqual(result, expected);
     });
-});
 
+    it("Should create a person", () => {
+        const result = Database.createPerson({ id: 333, name: "Yuu" });
+        deepEqual(result, true);
+    });
+
+    it("Should update a person", () => {
+        const result = Database.updatePerson(6504, { name: "Carlinha" });
+        deepEqual(result, true);
+    });
+
+    it("Should delete a person", () => {
+        const result = Database.deletePerson(5);
+        deepEqual(result, true);
+    });
+});
